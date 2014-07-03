12712c6e901230819269c9bb03ac190a3882b3a2
Simone <s.pellegrini11@campus.unimib.it>
2014-07-03 17:43:57 +0200

    added function getFastaString + comments

--- GeneUs.py
+++ GeneUs.py
@@ -54,26 +54,50 @@ def GTFParsing(gtfFile) :
 
 
 
-#import fasta file (returns single string)
+#imports fasta file (returns single string)
 def getFasta(path):
+    #opens file
     filefasta = open(path, 'r')
     lines = filefasta.readlines()
     filefasta.close()
-    #trim first line
+    #trims first line
     fasta = lines[1:len(lines)]
-    #remove \n
+    #removes '\n'
     for i in range(len(fasta)):
         fasta[i] = fasta[i].replace('\n', '')
     #characters per line
     cpl = len(fasta[1])
     return fasta, cpl
 
+#returns a substring of the fasta
+#fasta is used as a matrix (rows, character position)
+#unique characters-per-line value is supposed
+def getFastaString(begin, end, fasta, cpl):
+    fastastring = ''
+    #character position in the row is determined by its offset
+    offset_begin = begin % cpl
+    offset_end = end % cpl
+    #calculates the row that containes the character
+    index_begin = (begin - (offset_begin))/cpl
+    index_end = (end - (offset_end))/cpl
+    #temp vector of fasta
+    temp = fasta[index_begin:index_end]
+    #trims the offsets of the first and the last row
+    temp[0] = temp[0][offset_begin:]
+    temp[len(temp)-1] = temp[len(temp)-1][offset_end:]
+    #updates fastastring
+    for i in range(0,len(temp)):
+        fastastring = fastastring + temp[i]
+    return fastastring
+
 def demo():
     fasta, cpl = getFasta('ENm006.fa')
     exons, cds = GTFParsing('GAB3_annot.gtf');
-    print exons;
-    print cds;
-    print fasta, cpl
+    print 'exons:\n', exons;
+    print 'cds:\n', cds;
+    print 'fasta:\n', fasta, '\ncpl:\n', cpl
+    fastastring = getFastaString(1650, 2040, fasta, cpl)
+    print 'fastastring:\n', fastastring
 
 if __name__ == '__main__':
     demo()
